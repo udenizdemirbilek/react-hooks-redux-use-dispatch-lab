1a9a93ed439a830a98656ec077406b45b4444f71
demirbileku <udemirbilek2@gmail.com>
2021-07-21 17:54:56 +0300

    commit tree bfe747e0750d87857dca140ab3c908070ecd1e43

--- src/features/restaurant/RestaurantInput.js
+++ src/features/restaurant/RestaurantInput.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import { addRestaurant } from "./restaurantsSlice";
 
 function RestaurantInput() {
   const [name, setName] = useState("");
   const [location, setLocation] = useState("");
+  const dispatch = useDispatch()
 
   function handleNameChange(event) {
     setName(event.target.value);
@@ -16,6 +18,7 @@ function RestaurantInput() {
   function handleSubmit(event) {
     event.preventDefault();
     // add missing code
+    dispatch(addRestaurant({name, location}))
   }
 
   return (
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { createStore } from "redux";
 
 import restaurantsReducer from "./features/restaurant/restaurantsSlice";
 
-const store = createStore(restaurantsReducer);
+const store = createStore(restaurantsReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
   <Provider store={store}>
